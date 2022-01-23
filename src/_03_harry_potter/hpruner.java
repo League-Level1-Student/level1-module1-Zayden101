6df389ad3b50c76c958fd97ee77d5a5e4a6a6d37
Zayden101 <52078609+Zayden101@users.noreply.github.com>
2022-01-23 14:38:35 -0800

    b

---
+++ src/_03_harry_potter/hpruner.java
@@ -0,0 +1,14 @@
+package _03_harry_potter;
+
+public class hpruner {
+public static void main(String[] args) {
+	
+	HarryPotter hp = new HarryPotter();
+	
+	hp.makeInvisible(true);
+	hp.spyOnSnape();
+	hp.makeInvisible(false);
+	hp.castSpell("stupefy");
+	
+}
+}
