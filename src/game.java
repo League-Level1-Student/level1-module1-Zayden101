e552cf9792f6f4255883638ffc2eb6988c1ca262
Zayden101 <52078609+Zayden101@users.noreply.github.com>
2022-02-13 12:58:23 -0800

    p

---
+++ src/game.java
@@ -0,0 +1,13 @@
+import java.awt.AWTException;
+import java.awt.Robot;
+
+public class game {
+public static void main(String[] args) throws AWTException {
+	
+	Robot a = new Robot();
+	
+	
+	
+	
+}
+}
